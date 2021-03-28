c0fdd36b37025774497544b35d7cd839d05804d8
viniciussouzacss <50721086+viniciussouzacss@users.noreply.github.com>
2021-03-28 07:35:32 -0300

    feat: add HomePage with appBar and bottomNavigationBar.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -17,8 +17,149 @@ class homePage extends StatefulWidget {
 }
 
 class _homePageState extends State<homePage> {
+  String filterType = "tasks";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff815FC0),
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Tnotes",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            padding: EdgeInsets.only(right: 30.0),
+            icon: Icon(
+              Icons.account_circle,
+              color: Colors.white,
+              size: 30,
+            ),
+            onPressed: () {},
+            focusColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            hoverColor: Colors.transparent,
+            splashColor: Colors.transparent,
+          )
+        ],
+      ),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 110,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width,
+                        color: Color(0xff815FC0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.check_circle_sharp,
+                                      color: (filterType == "tasks")
+                                          ? Colors.white
+                                          : Colors.white.withOpacity(0.5),
+                                      size: 35,
+                                    ),
+                                    onPressed: () {
+                                      changeFilter("tasks");
+                                    },
+                                    focusColor: Colors.transparent,
+                                    highlightColor: Colors.transparent,
+                                    hoverColor: Colors.transparent,
+                                    splashColor: Colors.transparent,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: 40,
+                            ),
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.sticky_note_2_sharp,
+                                      color: (filterType == "notes")
+                                          ? Colors.white
+                                          : Colors.white.withOpacity(0.5),
+                                      size: 35,
+                                    ),
+                                    onPressed: () {
+                                      changeFilter("notes");
+                                    },
+                                    focusColor: Colors.transparent,
+                                    highlightColor: Colors.transparent,
+                                    hoverColor: Colors.transparent,
+                                    splashColor: Colors.transparent,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 65,
+                        width: 65,
+                        child: FittedBox(
+                          child: Theme(
+                            data: ThemeData(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                            ),
+                            child: FloatingActionButton(
+                              onPressed: () {
+                                // Add your onPressed code here!
+                              },
+                              child: const Icon(
+                                Icons.add,
+                                size: 32.5,
+                              ),
+                              backgroundColor: Colors.redAccent,
+                              focusColor: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                              splashColor: Colors.transparent,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  changeFilter(String filter) {
+    filterType = filter;
+    setState(() {});
   }
 }
