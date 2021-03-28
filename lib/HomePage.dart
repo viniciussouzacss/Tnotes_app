bd213b5fe641bb1f6eeb3c3e976caae31201af2d
viniciussouzacss <50721086+viniciussouzacss@users.noreply.github.com>
2021-03-27 22:30:40 -0300

    refactor(main): add MySplashScreen.

---
+++ lib/HomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'avenir'),
+      home: homePage(),
+    );
+  }
+}
+
+class homePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
